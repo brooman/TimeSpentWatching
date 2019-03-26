f794bfbaedb11a3af1c7eff7d63e4fdc287d1368
André Broman <laykith@gmail.com>
2019-03-26 14:17:04 +0100

    Changed SearchResultContainer to not show without prop

--- src/components/SearchResultContainer/index.js
+++ src/components/SearchResultContainer/index.js
@@ -4,23 +4,26 @@ import Styled from 'styled-components'
 import SearchResult from '../SearchResult'
 
 const StyledContainer = Styled.ul`
-  background-color: #222;
-  width: 100%;
-  padding: 2rem;
+background-color: #222;
+width: 80%;
+padding: 2rem;
+margin: 0;
 `
 
 const SearchResultContainer = props => {
+  if(!props.results) return <div />
+
   return (
     <StyledContainer>
-      {props.results && props.results.map((item) => {
+      {props.results.map((item) => {
         return <SearchResult name={item.name} />
       })}
     </StyledContainer>
   );
 };
-
-SearchResultContainer.propTypes = {
-  results: PropTypes.array
-};
-
-export default SearchResultContainer;
+  
+  SearchResultContainer.propTypes = {
+    results: PropTypes.array
+  };
+  
+  export default SearchResultContainer;
