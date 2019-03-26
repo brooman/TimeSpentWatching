53209117fd5c65e0661b95798cb940161832656e
André Broman <laykith@gmail.com>
2019-03-26 16:51:51 +0100

    Played around a bit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
+import Styled from 'styled-components'
 import AwesomeDebouncePromise from 'awesome-debounce-promise'
+import tmdb from '../../api/tmdb'
+
 import Search from '../Search'
 import SearchResultContainer from '../SearchResultContainer'
+import Header from '../Header'
 
-const StyledApp = styled.div`
+const AppContainer = Styled.div`
+  background-image: ${props => props.background ? tmdb.image(props.background) : tmdb.image('/gX8SYlnL9ZznfZwEH4KJUePBFUM.jpg')};
+`
+const Container = Styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -17,18 +23,19 @@ const searchAPI = query => {
   const url = `https://api.themoviedb.org/3/search/tv`;
   const queryString = `?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${encodeURIComponent(query)}`
   return fetch(url + queryString).then(res => res.json())
-};
+}
 
 const searchAPIDebounced = AwesomeDebouncePromise(searchAPI, 1000);
 
 class App extends React.Component
 {
   state = {
-    data: {},
+    timeSpent: 0, //Timespent in minutes
     searchText: "",
-    searchResult: null
+    searchResult: null,
+    myShows: null
   }
-
+  
   handleInput = async (event) => {
     this.setState({
       searchText: event.target.value, 
@@ -38,21 +45,27 @@ class App extends React.Component
     const result = await searchAPIDebounced(this.state.searchText);
 
     this.setState({
-      searchResult: result
+      searchResult: result,
+      //background: result.results    
     });
   }
 
   render(){
     return (
-      <StyledApp id="App">
-        <Search 
-          type="text"
-          onChange={this.handleInput}
-          value={this.state.searchText}
-          placeholder="Start searching for TV Show..."
-        />
-        {this.state.searchResult && <SearchResultContainer results={this.state.searchResult.results}></SearchResultContainer>}
-      </StyledApp>
+      <AppContainer id="App" background={this.state.background ? this.state.background : ''}>
+        <Container>
+          {this.state.myShows && <Header time="13 hours" />}
+
+          <Search 
+            type="text"
+            onChange={this.handleInput}
+            value={this.state.searchText}
+            placeholder="Start searching for TV Show..."
+          />
+
+          {this.state.searchResult && <SearchResultContainer results={this.state.searchResult.results} />}
+        </Container>
+      </AppContainer>
     )
   }
 
