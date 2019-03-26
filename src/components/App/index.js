0b312231a0c8af8820cd61e8563cabb4cdecaec4
André Broman <laykith@gmail.com>
2019-03-26 11:05:40 +0100

    Added App component

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #333;
+`
+
+class App extends React.Component
+{
+  render(){
+    return (
+      <StyledApp id="App">
+      
+      </StyledApp>
+    )
+  }
+}
+
+export default App;
