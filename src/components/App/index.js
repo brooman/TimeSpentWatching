6d08a17463fbb28661bc7e6893ff29f7d9f67630
André Broman <laykith@gmail.com>
2019-03-26 11:22:41 +0100

    Added searchbar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
+import Search from '../Search'
 
 const StyledApp = styled.div`
-  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100vw;
-  background-color: #333;
+  padding-top: 30vh;
 `
 
 class App extends React.Component
 {
+  state = {
+    data: {},
+    searchText: ""
+  }
+
+  handleInput(event){
+    this.setState({searchText: event.target.value});
+  }
+
   render(){
     return (
       <StyledApp id="App">
-      
+        <Search onChange={this.handleInput} value={this.state.searchText} />
       </StyledApp>
     )
   }
+
 }
 
 export default App;
